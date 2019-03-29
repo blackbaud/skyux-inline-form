c42e1d04597a30f88cedde1ac559a275e10f36f4
Alex Kingman <alex.kingman@blackbaud.com>
2019-03-29 10:53:41 -0400

    exporting types to root

--- src/app/public/modules/inline-form/index.ts
+++ src/app/public/modules/inline-form/index.ts
@@ -1,4 +1,4 @@
 export * from './inline-form-adapter.service';
 export * from './inline-form.component';
 export * from './inline-form.module';
-export * from './types/inline-form-close-args';
+export * from './types';
