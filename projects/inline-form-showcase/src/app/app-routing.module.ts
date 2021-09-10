46ab40af188bec36d32c2f666b8d8e3a79f3a57f
Alex <alex.kingman@blackbaud.com>
2021-09-10 13:44:22 -0400

    Next > Adopt Angular CLI (#53)
    * merge from master
    
    * skyux eject
    
    * updated dependencies
    
    * test
    
    * visual
    
    Co-authored-by: Alex Kingman <alex.kingman@blackbaud.me>

---
+++ projects/inline-form-showcase/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { SkyInlineFormDemoComponent } from './visual/inline-form/inline-form-demo.component';
+import { VisualComponent } from './visual/visual.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: VisualComponent
+  },
+  {
+    path: 'visual/inline-form',
+    component: SkyInlineFormDemoComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ projects/inline-form-showcase/src/app/visual/visual.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VisualComponent } from './visual.component';
+import { SkyInlineFormDemoComponent } from './inline-form/inline-form-demo.component';
+import { SkyTileDemoTileComponent } from './inline-form/inline-form-demo-tile.component';
+import { SkyE2eThemeSelectorModule } from '@skyux/e2e-client';
+import { SkyTilesModule } from '@skyux/tiles';
+import { SkyInlineFormModule } from 'projects/inline-form/src/public-api';
+import { RouterModule } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [
+    SkyInlineFormDemoComponent,
+    SkyTileDemoTileComponent,
+    VisualComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    SkyE2eThemeSelectorModule,
+    SkyInlineFormModule,
+    SkyTilesModule
+  ]
+})
+export class VisualModule { }
